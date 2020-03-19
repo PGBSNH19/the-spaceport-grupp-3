c9b019fedcde95d23d7c2ada29f37bb44c6fa0b7
simbasang <sebastian.englund@live.se>
2020-03-19 17:06:38 +0100

    @simbasang
    Co-Authored-By: Benjamin Ytterström <benytt@users.noreply.github.com>
    Co-Authored-By: Eric Sandberg <kaptenen@users.noreply.github.com>
    Co-Authored-By: Fredrik Odén <fredrikoden87@gmail.com>

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -8,45 +8,47 @@ namespace SpacePark
 
     class Program
     {
+        static async Task<string> CreatePersonFromAPI(string name) 
+        {
+            if (IsValidPerson(name).Result)
+            {
+                var person = new Person();
+                foreach (var item in results)
+                {
 
+                }
+
+            }
+            
+
+            return peopleResponse;
+
+        }
         static async Task<bool> IsValidPerson(string name)
         {
+            
+            
             var client = new RestClient("https://swapi.co/api/");
-<<<<<<< HEAD
-            var request =  new RestRequest($"people/?search={name}", DataFormat.Json);
+            var request = new RestRequest($"people/?search={name}", DataFormat.Json);
             var peopleResponse = client.Get<PersonSearch>(request);
-=======
-            var request = new RestRequest($"people/?search=Skywalker", DataFormat.Json);
-            var peopleResponse = client.Get<Person>(request);
-
-<<<<<<< HEAD
-                Console.WriteLine(response.result[0].name.ToString());
-=======
->>>>>>> f3b08a1749f6db4b8d35523271092fb16fd2fd5b
->>>>>>> 4cc84def53fbbfa85b967d7dac344451ca19f354
 
             foreach (var p in peopleResponse.Data.results)
             {
                 if (p.Name == name)
                 {
-                    
                     return true;
                 }
             }
             return false;
         }
 
-        static void CreatePersonFromAPI()
-        {
-
-        }
+       }
 
         static void Main(string[] args)
         {
-            var tempPerson = new Person { Name="Luke Skywalker" };
+           var parking =new SpacePark().IsValidPerson(x=> x.Name == Console.ReadLine()).creat
+
             Console.WriteLine(IsValidPerson(tempPerson.Name).Result);
-
-
             Console.WriteLine(IsValidPerson("Fel namn").Result);
 
 
