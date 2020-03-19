f3b08a1749f6db4b8d35523271092fb16fd2fd5b
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-03-19 16:10:20 +0100

    Hållt på med API
    Co-Authored-By: Benjamin Ytterström <benytt@users.noreply.github.com>
    Co-Authored-By: Fredrik Odén <fredrikoden87@gmail.com>
    Co-Authored-By: Eric Sandberg <kaptenen@users.noreply.github.com>
    Co-Authored-By: simbasang <simbasang@users.noreply.github.com>

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -1,28 +1,39 @@
 ﻿using RestSharp;
 using System.Threading.Tasks;
 using System;
+using System.Collections.Generic;
 
 namespace SpacePark
 {
 
     class Program
     {
+        static async Task<string> getapi()
+        {
+            List<Person> results = new List<Person>();
+            var client = new RestClient("https://swapi.co/api/");
+            var request = new RestRequest($"people/?search=Skywalker", DataFormat.Json);
+            var peopleResponse = client.Get<Person>(request);
+
+
+            foreach (var p in results)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            return null;
+        }
+
         static void Main(string[] args)
         {
-           static async Task<string> getapi()
-            {
-                var client =  new RestClient("https://swapi.co/api/");
-                var request = new RestRequest("starships", DataFormat.Json);
-                var response = client.Get(request);
-
-                Console.WriteLine(response.ToString());
-
-                return response.ToString();
-            }
 
             getapi();
 
+
+
         }
+
     }
+
 }
 
