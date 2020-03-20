f00805ecf8ad41e442d4d4004a820ae4b159ee39
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-03-20 13:01:07 +0100

    Skapade createPerson
    Co-Authored-By: Benjamin Ytterström <benytt@users.noreply.github.com>
    Co-Authored-By: Fredrik Odén <fredrikoden87@gmail.com>
    Co-Authored-By: Eric Sandberg <kaptenen@users.noreply.github.com>
    Co-Authored-By: simbasang <simbasang@users.noreply.github.com>

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -1,59 +1,23 @@
 ﻿using RestSharp;
 using System.Threading.Tasks;
 using System;
-using System.Collections.Generic;
 
 namespace SpacePark
 {
 
     class Program
     {
-        public static bool IsValidPerson(string name)
-        {
-            var response = CallToApi(($"people/?search={name}"));
-            foreach (var p in response.Data.results)
-            {
-                if (p.Name == name)
-                {
-                    Console.WriteLine(p.Name+ p.Starships[0]);
-                    return true;
-                }
-            }
-            return false;
-
-        }
-        public static IRestResponse<Result> CallToApi(string input)
-        {
-            var client = new RestClient("https://swapi.co/api/");
-            var request = new RestRequest(input, DataFormat.Json);
-            var apiResponse = client.Get<Result>(request);
-            return apiResponse;
-        }
-
-        public static void CreateStarshipFromAPI() 
-        {
-            //var client = new RestClient("https://swapi.co/api/");
-            //var request = new RestRequest($"people/?search=", DataFormat.Json);
-            //var peopleResponse = client.Get<PersonSearch>(request);
-        }
-
-        public static void CreatePersonFromAPI()  
-        {
-            
-        }
+        
 
          static void Main(string[] args)
         {
-            IsValidPerson("Chewbacca");
+            var spacePark = new SpacePark();
+            var temp = spacePark.IsValidPerson("Luke Skywalker");
+
+            Console.WriteLine(temp);
             Console.ReadLine();
         }
 
-    }
-
-    public class Result
-    {
-        
-        public List<Person> results { get; set; }
     }
 }
 
---
+++ Source/SpacePark/Result.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SpacePark
+{
+    public class Result
+    {
+        public List<Person> results { get; set; }
+    }
+}
+
